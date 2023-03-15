34f4fb345e657ee2ad2f9be144e4ccdd8731b686
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-03-15 12:17:20 +0000

    icons now attributed to sidebar, general page styling colour scheme adjusted

--- src/components/sidebar/sidebarButton.js
+++ src/components/sidebar/sidebarButton.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './sidebar.css';
+import './sidebarButton.css';
 
 export default function SidebarButton (props) {
     return (
-        <Link to>
-            <div>S</div>
+        <Link to={props.to}>
+            <div className="btn-body">
+                {props.icon}
+                <p className="btn-title">{props.title}</p>
+            </div>
         </Link>
     );
     
