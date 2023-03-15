67712e0a18e793211e6ea78ae9c1d89b4e0bd739
WarrenT <warrentyler1024@gmail.com>
2023-03-15 18:53:35 +0000

    Test get Spotify Token

--- src/screens/library/library.js
+++ src/screens/library/library.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+const CLIENT_ID = "3deed9bc4aff411493d9447b7d93fdc6";
+const CLIENT_SECRET = "36756a1ae5a5415594e0eda5bc0508b9";
 
 export default function Library() {
-    return (
-        <div className="screen-conatiner">Library</div>
-    )
+  // run once when app starts
+  useEffect(() => {
+    // Get Spotify API Token
+    const tokenParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+    };
+
+    fetch("https://accounts.spotify.com/api/token", tokenParams)
+      .then((result) => result.json())
+      .then((data) => console.log(data.access_token));
+  }, []);
+
+  return <div className="screen-conatiner">Library</div>;
 }
