012fcf1add8e9055e2e516469ef0d035a44fc1ef
WarrenT <warrentyler1024@gmail.com>
2023-03-15 19:12:58 +0000

    Spotify API search test in Library

--- src/screens/library/library.js
+++ src/screens/library/library.js
@@ -3,18 +3,21 @@ import { useState, useEffect } from "react";
 // needed for react bootsrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
-    Container,
-    InputGroup,
-    FormControl,
-    Button,
-    Row,
-    Card,
-  } from "react-bootstrap";
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Row,
+  Card,
+} from "react-bootstrap";
 
 const CLIENT_ID = "3deed9bc4aff411493d9447b7d93fdc6";
 const CLIENT_SECRET = "36756a1ae5a5415594e0eda5bc0508b9";
 
 export default function Library() {
+  const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+
   // run once when app starts
   useEffect(() => {
     // Get Spotify API Token
@@ -31,5 +34,58 @@ export default function Library() {
       .then((data) => console.log(data.access_token));
   }, []);
 
-  return <div className="screen-conatiner">Library</div>;
+  async function search() {
+    console.log(accessToken);
+    console.log(`Search for ${searchInput}`);
+
+    // get request using search, to get the Artist ID
+    const artistParams = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+
+    let artistID = await fetch(
+      `https://api.spotify.com/v1/search?q=${searchInput}&type=artist`,
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => console.log(data));
+
+    // get request with Artist ID, to get all the albums of that Artist
+    // display those Albums to the user
+  }
+
+  return (
+    <div className="screen-conatiner">
+      Library
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Search for Artist"
+            type="input"
+            onKeyUp={(event) => {
+              if (event.key === "Enter") {
+                search();
+              }
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
+          />
+          <Button onClick={search}>Search</Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className="row row-cols-12 mx-2">
+          <Card>
+            <Card.Img src="#" />
+            <Card.Body>
+              <Card.Title>Album name</Card.Title>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
+    </div>
+  );
 }
