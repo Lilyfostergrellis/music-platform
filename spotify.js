cd8d4534b444b7a67993bc1deeca9ba01dbb73c9
gmuchiza <gmuchiza@ymail.com>
2023-03-15 20:40:07 +0000

    deleted unnecessary file

--- spotify.js
+++
@@ -1,6 +0,0 @@
-const authEndpoint = "https://accounts.spotify.com/authorize?";
-const clientID = "cb173b28d2a84091ade448f19d443b38";
-const redirectUri = "https://localhost:3000";
-const scopes =["user-library-read", "playlist-read-private"];
-
-export const loginEndpoint = `${authEndpoint}client_id=${clientID}&redirect_url=${redirectUri}&scope=${scopes.join("&20")}&response_type=token&show_dialog=true`;
