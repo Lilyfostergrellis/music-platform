42041d60592cf41b13a8e3e427e41d379b4339f4
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-03-21 20:57:09 +0000

    new-releases.js file - api token attributed on page loading

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Library from "../library/library";
 import Feed from "../feed/feed";
-import Trending from "../trending/trending";
 import Player from "../player/player";
 import Favourites from "../favourites/favourites";
 import "./home.css";
 import Sidebar from "../../components/sidebar";
 import { Box } from "@chakra-ui/react";
+import NewReleases from "../new-releases/new-releases";
 //route paths set for js files/pages of application.
 
 export default function Home() {
@@ -18,7 +18,7 @@ export default function Home() {
         <Routes>
           <Route path="/library" element={<Library />} />
           <Route path="/feed" element={<Feed />} />
-          <Route path="/trending" element={<Trending />} />
+          <Route path="/new-releases" element={<NewReleases/>} />
           <Route path="/player" element={<Player />} />
           <Route path="/favourites" element={<Favourites />} />
         </Routes>
