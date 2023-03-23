1525877764e3b53845c66c432705eec389b437b1
gmuchiza <gmuchiza@ymail.com>
2023-03-23 20:29:29 +0000

    display genres

--- src/screens/feed/Feed.js
+++ src/screens/feed/Feed.js
@@ -1,7 +1,90 @@
-import React from "react";
-
-export default function Feed() {
+import {
+    Heading,
+    SimpleGrid,
+    Image,
+    Card,
+    CardBody,
+    CardHeader,
+    Box,
+    Text,
+  } from "@chakra-ui/react";
+  import React from "react";
+  import { useState, useContext, useEffect } from "react";
+  import AppContext from "../../context/appContext";
+  
+  export default function Feed() {
+    // get access token from react context
+    const ctx = useContext(AppContext);
+    const [albums, setAlbums] = useState([]);
+  
+    useEffect(() => {
+      console.log("new releases-useEffect" + ctx.accessToken);
+  
+      // get request using search, to get the Artist ID
+      const searchParams = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${ctx.accessToken}`,
+        },
+      };
+  
+      // get request for new releases
+      fetch(
+        `https://api.spotify.com/v1/recommendations/available-genre-seeds`,
+        searchParams
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setAlbums(data.genres);
+        });
+    }, []); 
+  
+    // async function search() {
+    //   console.log("new releases" + ctx.accessToken);
+  
+    //   // get request using search, to get the Artist ID
+    //   const searchParams = {
+    //     method: "GET",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: `Bearer ${ctx.accessToken}`,
+    //     },
+    //   };
+  
+    //   // get request for new releases
+    //   await fetch(
+    //     `https://api.spotify.com/v1/browse/new-releases?country=US&limit=10&offset=5`,
+    //     searchParams
+    //   )
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       console.log(data);
+    //       return setAlbums(data.albums.items);
+    //     });
+    // }
+  
     return (
-        <div className="screen-container">Feed</div>
-    )
-}
+      <>
+        <Heading>New Releases</Heading>
+        <Box className="searchresults">
+          <SimpleGrid minChildWidth="160px" spacing="40px">
+            {
+              // display Albums to the user
+              albums &&
+                albums.map((album) => {
+                  console.log(album);
+  
+                  return (
+                    
+                      <Text>{album}</Text>
+                  );
+                })
+            }
+          </SimpleGrid>
+        </Box>
+      </>
+    );
+  }
+  
